064f4fe8d16bbadb39929530db7b6d1542a49b29
fans656 <fans656@yahoo.com>
2025-03-06 23:29:13 +0800

    login & status

--- duf/cli/api.py
+++ duf/cli/api.py
@@ -4,24 +4,49 @@ import json
 import click
 import requests
 
+from .cons import paths
+
 
 class API:
 
     def __init__(self):
         self.origin = 'https://duf.fans656.me'
 
-    def post(self, path, req: dict = {}):
+    def get(self, *args, **kwargs):
+        return self.request('GET', *args, **kwargs)
+
+    def post(self, *args, **kwargs):
+        return self.request('POST', *args, **kwargs)
+
+    def request(self, method, path, req: dict = {}, raw: bool = False):
         if path.startswith('http'):
             url = path
         else:
             url = f'{self.origin}{path}'
-        res = requests.post(
-            url,
-            json=req,
-            cookies={'token': os.environ.get('TOKEN')},
-        )
+
+        cookies = {}
+        if paths.token.exists():
+            with paths.token.open() as f:
+                cookies['token'] = f.read()
+
+        if method == 'GET':
+            res = requests.get(
+                url,
+                params=req,
+                cookies=cookies,
+            )
+        else:
+            res = requests.post(
+                url,
+                json=req,
+                cookies=cookies,
+            )
+
         if res.status_code == 200:
-            return res.json()
+            if raw:
+                return res
+            else:
+                return res.json()
         else:
             try:
                 click.echo(json.dumps(res.json(), indent=2))
